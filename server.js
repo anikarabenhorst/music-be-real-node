b88fc80b7dc559441b259158292462d07359bf0e
shanemitnick <shane.mitnick21@gmail.com>
2022-04-19 10:04:39 -0400

    new routes, update/delete user and post, post last 5

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import userController from './controllers/user-controller.js';
 import postController from './controllers/post-controller.js';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 8000;
 
 userController(app);
 postController(app);
-app.listen(port, () => {
-  console.log( `now lisetning on port ${port}`)
-});
+
+app.listen(port, () => { console.log( `now lisetning on port ${port}`)});
