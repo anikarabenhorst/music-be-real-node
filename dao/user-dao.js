db198da963efbe7d76b279cefe6ae2c07c496f40
shanemitnick <shane.mitnick21@gmail.com>
2022-04-26 22:58:26 -0400

    updateUser functionality, updated userSchema, cleaned postsDao up

--- dao/user-dao.js
+++ dao/user-dao.js
@@ -3,12 +3,6 @@ import userModel from "../models/user-model.js";
 const findAllUsers = () => userModel.find();
 const createUser = (user) => userModel.create(user);
 const deleteUser = (uid) => userModel.deleteOne({_id: uid});
-
-
-const updateUser = async (uid, updates) => {
-  let prevUser = await userModel.findOne({_id: uid}).exec();
-  prevUser.stats = updates.stats
-  prevUser.save();
-};
+const updateUser = (uid, user) => userModel.updateOne({_id: uid}, {$set: user});
 
 export default {findAllUsers, createUser, deleteUser, updateUser};
