42c7fb23ef9c5bfd1936ca93c178cf755464c395
shanemitnick <shane.mitnick21@gmail.com>
2022-04-28 13:59:48 -0400

    updated post schema

--- schemas/posts-schema.js
+++ schemas/posts-schema.js
@@ -1,25 +1,22 @@
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-  userId: String,
-  postedBy: {
-    username: String
-  },
-  liked: { type: Boolean, defaultValue: false },
-  disliked: { type: Boolean, defaultValue: false },
-  timePosted: String,
-  caption: String,
-  trackId: String,
-  stats: {
-    comments: { type: Number, defaultValue: 0 },
-    likes: { type: Number, defaultValue: 0 },
-    dislikes: { type: Number, defaultValue: 0 }
-  },
+  userId: {type: String, required: true},
+  liked: {type: Array, default: []},
+  disliked: {type: Array, default: []},
+  caption: {type: String, default: ""},
+  date: {type: Date, default: Date.now()},
   comments: [{
     handle: String,
     avatarImage: String,
     comment: String,
-  }]
+  }],
+  song_title: {type: String, required: true},
+  track_id: {type: Number, required: true},
+  artist_name: {type: String, required: true},
+  album_cover: {type: String, required: true},
+  album_name: {type: String, required: true}
+
 } , {collection: 'posts'});
 
 export default postSchema;
